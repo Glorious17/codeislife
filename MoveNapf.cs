791bdfbd9024602e318f0caaa8e06d746af26ab3
nonononoki <ndinh@hs-bremen.de>
2016-01-15 11:56:01 +0100

    - Noki

---
+++ MoveNapf.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveNapf : MonoBehaviour {
+
+	private Rigidbody rb;
+	private float z = 15f;
+	public int speed = 5; 
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+
+	void OnMouseDrag()
+	{
+		Vector3 v3 = Input.mousePosition;
+		//v3.y = 0;
+		v3.z = z;
+		v3 = Camera.main.ScreenToWorldPoint(v3);
+
+		rb.velocity = (v3 - gameObject.transform.position) * speed;
+	}
+
+
+}
